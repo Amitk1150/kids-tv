78640bfb6342628f210475a4af1b67e085aef87f
Amit Kumar <FAmitkumar@schools.nyc.gov>
2024-05-25 14:24:49 -0400

    ok

--- src/components/YoutubeEmbed/index.js
+++ src/components/YoutubeEmbed/index.js
@@ -4,8 +4,8 @@ import "./style.scss";
 const YoutubeEmbed = ({ src }) => (
   <div className="video-responsive">
     <iframe
-      height="auto"
-      width="350px"
+      height="314px"
+      width="380px"
       src={src}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
